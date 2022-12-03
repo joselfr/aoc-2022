66b4f495fa8c226e51e3d2fa613bcb9f0097025d
José Rodrigues <jro@witbe.net>
2022-12-03 22:28:50 +0100

    Day02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -6,7 +6,6 @@ fn part1(file_path: &str) -> i32 {
 
     input.split('\n').for_each(|round| {
         if let &[a, b] = round.split_whitespace().collect::<Vec<&str>>().as_slice() {
-            println!("{} VS {}", a, b);
             match b {
                 "X" => {
                     score += 1;
@@ -39,7 +38,49 @@ fn part1(file_path: &str) -> i32 {
     return score;
 }
 
+fn part2(file_path: &str) -> i32 {
+    let input = fs::read_to_string(file_path).expect("Unable to read file");
+    let mut score = 0;
+
+    input.split('\n').for_each(|round| {
+        if let &[a, b] = round.split_whitespace().collect::<Vec<&str>>().as_slice() {
+            match b {
+                "X" => {
+                    match a {
+                        "A" => score += 3,
+                        "B" => score += 1,
+                        "C" => score += 2,
+                        _ => (),
+                    }
+                }
+                "Y" => {
+                    score += 3;
+                    match a {
+                        "A" => score += 1,
+                        "B" => score += 2,
+                        "C" => score += 3,
+                        _ => (),
+                    }
+                }
+                "Z" => {
+                    score += 6;
+                    match a {
+                        "A" => score += 2,
+                        "B" => score += 3,
+                        "C" => score += 1,
+                        _ => (),
+                    }
+                }
+                _ => ()
+            }
+        }
+    });
+    return score;
+}
+
 fn main() {
     assert_eq!(part1("./inputs/example"), 15);
     assert_eq!(part1("./inputs/input"), 11449);
+    assert_eq!(part2("./inputs/example"), 12);
+    assert_eq!(part2("./inputs/input"), 13187);
 }
